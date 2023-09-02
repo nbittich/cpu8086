36759999858c1e4e406bf970155e186b4db34072
Nordine Bittich <nordine@bittich.be>
2023-09-02 14:50:18 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 const OP_MOV: u8 = 0b100010;
+const REGISTER_TO_REGISTER: u8 = 0b11;
 
 fn main() {
     let path: PathBuf = args().skip(1).take(1).map(PathBuf::from).collect();
@@ -15,47 +16,51 @@ fn main() {
         .expect("could not open file");
 
     let mut reader = BufReader::new(file);
-    let mut buffer = [0u8; 2];
+    let mut buffer = [0u8; 1];
 
     println!("bits 16");
     println!();
 
     while let Ok(_) = reader.read_exact(&mut buffer) {
         let byte_1 = buffer[0];
-        let byte_2 = buffer[1];
 
         let op_code = byte_1 >> 2; // opcode
-        let d = (byte_1 >> 6) & 1; // direction is to register / direction is from register
-        let w = byte_1 & 1; // word/byte operation
-
-        let reg_mode = byte_2 >> 6; // register mode / memory mode
-        let reg = register_table((byte_2 >> 3) & 7, w); // register operand / source
-        let rm = register_table(byte_2 & 7, w); // register operand / dest
-
-        // it should be the opposite, but nasm always return 0
-        // if d is 0, instruction source is specified in reg field, else if d is 1 in rm field
-        let (source, dest) = if d == 0 { (reg, rm) } else { (rm, reg) };
-        if cfg!(debug_assertions) {
-            println!("############# DEBUG ##############");
-            println!("{byte_1:b} {byte_2:b}");
-            println!("{byte_1:b} {byte_2:b}");
-            println!("opcode : {op_code:b}");
-            println!("d : {d:b}");
-            println!("w : {w:b}");
-            println!("reg_mod : {reg_mode:b}");
-            println!("reg: {reg}");
-            println!("rm: {rm}");
-            println!("############# DEBUG ##############");
-        }
 
         if op_code == OP_MOV {
-            print!("mov ");
-        } else {
-            panic!("opcoded {op_code:b} not implemented");
-        }
-        print!("{dest}, {source}");
+            let d = (byte_1 >> 6) & 1; // direction is to register / direction is from register
+            let w = byte_1 & 1; // word/byte operation
+            reader
+                .read_exact(&mut buffer)
+                .expect("op mov requires an extra byte");
+            let byte_2 = buffer[0];
+            let reg_mode = byte_2 >> 6; // register mode / memory mode
 
-        println!();
+            if cfg!(debug_assertions) {
+                println!("############# DEBUG ##############");
+                println!("{byte_1:b} {byte_2:b}");
+                println!("{byte_1:b} {byte_2:b}");
+                println!("opcode : {op_code:b}");
+                println!("d : {d:b}");
+                println!("w : {w:b}");
+                println!("reg_mod : {reg_mode:b}");
+            }
+
+            if reg_mode == REGISTER_TO_REGISTER {
+                let reg = register_table((byte_2 >> 3) & 7, w); // register operand / source
+                let rm = register_table(byte_2 & 7, w); // register operand / dest
+                if cfg!(debug_assertions) {
+                    println!("reg: {reg}");
+                    println!("rm: {rm}");
+                }
+                // it should be the opposite, but nasm always return 0
+                // if d is 0, instruction source is specified in reg field, else if d is 1 in rm field
+                let (source, dest) = if d == 0 { (reg, rm) } else { (rm, reg) };
+                println!("mov {dest}, {source}");
+            }
+            if cfg!(debug_assertions) {
+                println!("############# DEBUG ##############");
+            }
+        }
     }
 }
 
